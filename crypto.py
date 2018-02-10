6cd3aeea801e17c5921951e1cc3af81e43a7a7ff
Nazif10 <nazif.ahmed12@gmail.com>
2018-02-10 21:17:29 +1100

    Added historical price access function

--- crypto.py
+++ crypto.py
@@ -6,18 +6,32 @@ import json
 #import pandas as pd
 #import matplotlib.pyplot as plt
 
-def price(symbol, comparison_symbols=['USD'], exchange=''):
+
+def price(symbol, comparison_symbols=['USD','AUD'], exchange=''):
     url = 'https://min-api.cryptocompare.com/data/price?fsym={}&tsyms={}'\
-            .format(symbol.upper(), ','.join(comparison_symbols).upper())
-    if exchange:
-        url += '&e={}'.format(exchange)
+            .format(symbol.upper(),','.join(comparison_symbols).upper())
+    # if exchange:
+    #     url += '&e={}'.format(exchange)
     page = requests.get(url)
     data = page.json()
+    print(data)
     return data
 
-def calcGainz(int investment,):
-	ret = price('ltc',exchange='Poloniex')
-	print(ret)
 
-	gainz = 1000/int(ret['USD'])
-	print(gainz)
+# price('eth')
+
+def historical(symbol,comparison_symbols=['USD'],time_stamp=1452680400):
+
+    url = 'https://min-api.cryptocompare.com/data/pricehistorical?fsym={}&tsyms={}&ts={}'\
+            .format((symbol.upper()),','.join(comparison_symbols).upper(),time_stamp)
+    page = requests.get(url)
+    data = page.json()
+    print(data)
+
+historical('ETH')
+# def calcGainz(int investment,):
+# 	ret = price('ltc',exchange='Poloniex')
+# 	print(ret)
+
+# 	gainz = 1000/int(ret['USD'])
+# 	print(gainz)
